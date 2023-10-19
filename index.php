dee7475aea92c3487d9973e9507372aaa9da131f
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-10-19 14:47:02 +0100

    add method objects and print

--- index.php
+++ index.php
@@ -9,8 +9,25 @@ class Movie
     // Costruttore
     public function __construct($title, $language, $generes) 
     {
-      $this-> title = $title;
-      $this-> language = $language;
-      $this-> generes = $generes;
+        $this-> title = $title;
+        $this-> language = $language;
+        $this-> generes = $generes;
+    }
+    // Metodo per ottenere i dettagli del film
+    public function getDetails() 
+    {
+      return "Titolo: " . 
+      $this->title . ", Lingua: " . $this->language . ", Genere: " . $this->generes;
     }
 }
+
+$movie_1 = new Movie ('Rambo', 'Inglese', 'Azione');
+$movie_2 = new Movie ('Romanzo Criminale', 'Italiano', 'Thriller');
+$movie_3 = new Movie ('The Witcher', 'Inglese', 'Fantascienza');
+var_dump($movie_3);
+
+echo $movie_1->getDetails();
+echo "<br><br>";
+echo $movie_2->getDetails();
+echo "<br><br>";
+echo $movie_3->getDetails();
